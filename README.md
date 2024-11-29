8ad38e9a221575b0b2fbde81b92cdf08355ef318
r0ute <route-a@ya.ru>
2024-11-29 17:45:59 -0600

    init project

---
+++ README.md
@@ -0,0 +1,31 @@
+# RackAutoFill
+
+## BoxSize
+
+| Name | Dimensions | Cubic Volume | Description             |
+| ---- | ---------- | -----------: | ----------------------- |
+| S-C  | 8x8x8      |          512 | Small Cube              |
+| S-R  | 20x10x7    |         1400 | Small Rectangular       |
+| M-CM | 15x15x15   |         3375 | Medium Compact Cube     |
+| M-FR | 20x20x10   |         4000 | Medium Flat Rectangular |
+| M-C  | 20x20x20   |         8000 | Medium Cube             |
+| L-R  | 30x20x20   |        12000 | Large Rectangular       |
+| XL-R | 40x26x26   |        26080 | Extra Large Rectangular |
+
+## DisplayType
+
+| Abbreviation | Display Type | Color (HTML Code)                                         |
+| ------------ | ------------ | --------------------------------------------------------- |
+| F            | FREEZER      | <span style="color:#A3D8FF">Icy Blue (#A3D8FF)</span>     |
+| R            | FRIDGE       | <span style="color:#B3E0E5">Pale Cyan (#B3E0E5)</span>    |
+| C            | CRATE        | <span style="color:#D1A15A">Wooden Brown (#D1A15A)</span> |
+| S            | SHELF        | <span style="color:#D3D3D3">Neutral Gray (#D3D3D3)</span> |
+
+## ProductCategory
+
+| Abbreviation | Product Category | Color (HTML Code)                                            |
+| ------------ | ---------------- | ------------------------------------------------------------ |
+| E            | EDIBLE           | <span style="color:#A3D77A">Leafy Green (#A3D77A)</span>     |
+| D            | DRINK            | <span style="color:#67BFFF">Refreshing Blue (#67BFFF)</span> |
+| C            | CLEANING         | <span style="color:#F0F8FF">Clean White (#F0F8FF)</span>     |
+| B            | BOOK             | <span style="color:#C2B29A">Calming Brown (#C2B29A)</span>   |
---
+++ src/Plugin.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Logging;
+using HarmonyLib;
+using MyBox;
+using TMPro;
+using UnityEngine;
+
+namespace SS.src;
+
+[BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+public class Plugin : BaseUnityPlugin
+{
+    internal static new ManualLogSource Logger;
+
+    internal static ConfigEntry<bool> AutofillRack;
+
+    internal static ConfigEntry<KeyboardShortcut> ForceAutofillRackKey;
+
+    private void Awake()
+    {
+        // Plugin startup logic
+        Logger = base.Logger;
+
+        AutofillRack = Config.Bind("General", "AutofillRack", true, "Enable to rack on day cycle change");
+
+        ForceAutofillRackKey = Config.Bind("Key Bindings", "ForceAutofillRackKey",
+                new KeyboardShortcut(KeyCode.E, KeyCode.LeftControl));
+
+        Harmony harmony = new(MyPluginInfo.PLUGIN_GUID);
+        harmony.PatchAll(typeof(Patches));
+
+        Logger.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
+    }
+
+
+    class Patches
+    {
+
+        [HarmonyPatch(typeof(PricingProductViewer), nameof(PricingProductViewer.UpdateUnlockedProducts))]
+        [HarmonyPostfix]
+        static void OnUpdateUnlockedProducts(int licenseID)
+        {
+        }
+
+        [HarmonyPatch(typeof(DayCycleManager), nameof(DayCycleManager.StartNextDay))]
+        [HarmonyPostfix]
+        static void OnNextDay()
+        {
+        }
+
+        [HarmonyPatch(typeof(DayCycleManager), "Start")]
+        [HarmonyPostfix]
+        static void OnDayStart()
+        {
+        }
+
+        [HarmonyPatch(typeof(DayCycleManager), "Update")]
+        [HarmonyPostfix]
+        static void OnDayUpdate()
+        {
+            if (ForceAutofillRackKey.Value.IsDown())
+            {
+                AutofillRack(false);
+            }
+        }
+
+
+        [HarmonyPatch(typeof(PricingItem), nameof(PricingItem.Setup))]
+        [HarmonyPostfix]
+        static void OnPricingItemSetup(Pricing data, ref PricingItem __instance)
+        {
+
+
+        }
+
+        private static void AutofillRack(bool auto = true)
+        {
+            if (auto && !Plugin.AutofillRack.Value)
+            {
+                return;
+            }
+
+
+            if (!auto)
+            {
+                Singleton<SFXManager>.Instance.PlayCoinSFX();
+            }
+
+            Logger.LogInfo($"Rack autofill finished: auto={auto}");
+
+        }
+
+
+    }
+}
